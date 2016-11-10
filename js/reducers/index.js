ee3e200ac2ba53e0c3e0b1cbe6a00ef0d2591a96
supriya <supriya@supriyas-MacBook-Air.local>
2016-11-10 12:04:44 +0530

    files deleted

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -3,12 +3,10 @@ import { combineReducers } from 'redux';
 
 import drawer from './drawer';
 import cardNavigation from './cardNavigation';
-import route from './route';
 
 export default combineReducers({
 
   drawer,
   cardNavigation,
-  route,
 
 });
