269d84a50500235b036175e3a522d5d28f990f26
Shivraj <shivraj@geekyants.com>
2017-07-03 12:45:09 +0530

    merge

--- android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
+++ android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
@@ -3,6 +3,7 @@ package com.nativebasekitchensink;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new CodePush(null, getApplicationContext(), BuildConfig.DEBUG)
       );
     }
