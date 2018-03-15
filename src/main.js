fdd279b342a59ab80368bffa9b756eb7aa86e8bd
Shivraj <shivraj@geekyants.com>
2018-03-15 16:17:59 +0530

    missing file added

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { AppRegistry } from "react-native";
-import App from "./App";
+import App from "./boot/setup";
 AppRegistry.registerComponent("NativebaseKitchenSink", () => App);
 AppRegistry.runApplication("NativebaseKitchenSink", {
   rootTag: document.getElementById("root")
