5af414b79c00147e65b8a78cd426e0078afc6e40
Supriya Kalghatgi <supriya@sahusoft.com>
2018-04-11 10:35:55 +0530

    Update setup.js

--- src/boot/setup.js
+++ src/boot/setup.js
@@ -1,4 +1,3 @@
-import * as Expo from "expo";
 import React, { Component } from "react";
 import { StyleProvider } from "native-base";
 
@@ -7,27 +6,7 @@ import getTheme from "../theme/components";
 import variables from "../theme/variables/commonColor";
 
 export default class Setup extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isReady: false
-    };
-  }
-  componentWillMount() {
-    this.loadFonts();
-  }
-  async loadFonts() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
-    });
-    this.setState({ isReady: true });
-  }
   render() {
-    if (!this.state.isReady) {
-      return <Expo.AppLoading />;
-    }
     return (
       <StyleProvider style={getTheme(variables)}>
         <App />
