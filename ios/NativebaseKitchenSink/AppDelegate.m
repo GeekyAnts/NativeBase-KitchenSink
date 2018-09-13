704663b5a7a75e9afc87dd32a5a0f489faad0d9e
Peter Keller <petekeller2@gmail.com>
2018-09-13 19:42:46 -0400

    Fix orientation change display issue See commit 20945 in react native

--- ios/NativebaseKitchenSink/AppDelegate.m
+++ ios/NativebaseKitchenSink/AppDelegate.m
@@ -22,7 +22,8 @@
                                                       moduleName:@"NativebaseKitchenSink"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
+  rootView.backgroundColor = [UIColor blackColor];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
