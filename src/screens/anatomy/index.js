d13742d74fd790c5fdb9b980cd18d0bd6da02b52
Supriya Kalghatgi <supriya@Supriyas-Air.blr-office.geekyants.com>
2018-09-27 14:56:17 +0530

    versionCode 6, change in anatomy

--- src/screens/anatomy/index.js
+++ src/screens/anatomy/index.js
@@ -36,7 +36,7 @@ class Anatomy extends Component {
         </Header>
 
         <Content padder>
-          <Text>Content goes here!</Text>
+          <Text>Content goes here !!</Text>
         </Content>
 
         <Footer>
