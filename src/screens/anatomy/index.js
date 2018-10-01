4f2b84e88e718a186bee0ed2233d91fc0c30ce66
Supriya Kalghatgi <supriya@Supriyas-Air.blr-office.geekyants.com>
2018-10-01 12:14:33 +0530

    versionCode 16, versionName 2.4.0, beta

--- src/screens/anatomy/index.js
+++ src/screens/anatomy/index.js
@@ -36,7 +36,7 @@ class Anatomy extends Component {
         </Header>
 
         <Content padder>
-          <Text>Content goes here (alpha)</Text>
+          <Text>Content goes here (beta)</Text>
         </Content>
 
         <Footer>
