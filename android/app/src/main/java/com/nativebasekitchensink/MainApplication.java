3e8e866216fa2a905d0e2138600c41387eacc709
Supriya Kalghatgi <supriya@geekyants.com>
2019-02-19 12:45:04 +0530

    Master fixed and working for iOS

--- android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
+++ android/app/src/main/java/com/nativebasekitchensink/MainApplication.java
@@ -1,9 +1,8 @@
-package io.market.nativebase.geekyants.nativebasekitchensink;
+package com.nativebasekitchensink;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -23,8 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
 
